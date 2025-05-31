8e19ae99ebff96614dd383feb9e8fca26f87f1eb
Thierry Varela <varelathierry@gmail.com>
2025-05-31 16:07:38 -0300

    feat: lists and arrays

--- Program.cs
+++ Program.cs
@@ -16,13 +16,13 @@ using System.Text;
 //double receivedWageCarl = carl.ReceiveWage();
 //Console.WriteLine($"Wage paid (message from Program): {receivedWageCarl}");
 
-Console.WriteLine("Creating an employee!");
-Console.WriteLine("--------------------\n");
+//Console.WriteLine("Creating an employee!");
+//Console.WriteLine("--------------------\n");
 
-Employee gama = new("Gama", "Odlared", "al@gmail.br", new DateTime(1996, 1, 21), 40, EmployeeType.Research);
+//Employee gama = new("Gama", "Odlared", "ga@gmail.br", new DateTime(1996, 1, 1), 40, EmployeeType.Research);
 
-string gamaAsJson = gama.ConvertToJson();
-Console.WriteLine(gamaAsJson);
+//string gamaAsJson = gama.ConvertToJson();
+//Console.WriteLine(gamaAsJson);
 
 //gama.DisplayEmployeeDetails();
 
@@ -35,9 +35,15 @@ Console.WriteLine(gamaAsJson);
 //int receivedBonus = gama.CalculateBonus(minimalBonus);
 //Console.WriteLine($"The minimum bonus is {minimalBonus}, and {gama.firstName} has received a bonus of {receivedBonus}");
 
-int minimalBonus = 200;
-int bonusTax = 0;
-int receivedBonus = gama.CalculateBonusAndBonusTax(minimalBonus, ref bonusTax);
+//int minimalBonus = 200;
+//int bonusTax = 0;
+//int receivedBonus = gama.CalculateBonusAndBonusTax(minimalBonus, ref bonusTax);
+
+//Employee.ChangeTaxRate(0.2);
+
+//gama.ReceiveWage();
+
+//gama.DisplayEmployeeDetails();
 
 //var example = gama.ExampleOut(out int bonusExample);
 //Console.WriteLine(example); // 100
@@ -63,7 +69,71 @@ int receivedBonus = gama.CalculateBonusAndBonusTax(minimalBonus, ref bonusTax);
 //string result = builder.ToString();
 //Console.WriteLine(result);
 
-WorkTask task;
-task.description = "Climbing hard routes";
-task.hours = 4;
-task.PerformWorkTask();
+//WorkTask task;
+//task.description = "Climbing hard routes";
+//task.hours = 4;
+//task.PerformWorkTask();
+
+//Customer customer = new();
+
+//gama.CalculateWage();
+
+//Account account = new("1234");
+
+//int[] sampleArray = new int[5];
+
+//int[] sampleArray2 = new int[] { 1, 2, 3, 4, 5 };
+
+//Console.WriteLine("How much books do you want to borrow?");
+
+//int length = int.Parse(Console.ReadLine());
+
+//string[] inputArray = new string[length];
+
+//for (int i = 0; i < inputArray.Length; i++)
+//{
+//    Console.WriteLine("Write the name of the book:");
+//    inputArray[i] = Console.ReadLine();
+//}
+
+//Console.WriteLine("Books List:");
+
+//for (int i = 0;i < inputArray.Length; i++)
+//{
+//    Console.WriteLine($"{i} - {inputArray[i]}");
+//}
+//Employee alfa = new("Alfa", "Odlared", "al@gmail.br", new DateTime(1996, 1, 2), 40, EmployeeType.Research);
+//Employee beta = new("Beta", "Odlared", "be@gmail.br", new DateTime(1996, 1, 3), 40, EmployeeType.Research);
+//Employee delta = new("Delta", "Odlared", "de@gmail.br", new DateTime(1996, 1, 4), 40, EmployeeType.Research);
+//Employee horus = new("Horus", "Odlared", "ho@gmail.br", new DateTime(1996, 1, 5), 40, EmployeeType.Research);
+
+//Employee[] employees = new Employee[5];
+
+//employees[0] = alfa;
+//employees[1] = beta;
+//employees[2] = delta;
+//employees[3] = horus;
+//employees[4] = gama;
+
+//foreach (Employee employee in employees)
+//{
+//    employee.DisplayEmployeeDetails();
+//    var numberOfHoursWorked = new Random().Next(40);
+//    employee.PerformWork(numberOfHoursWorked);
+//    employee.ReceiveWage();
+//}
+
+// list is a type of collections
+
+List<int> employeeIds = new List<int>();
+
+employeeIds.Add(21);
+employeeIds.Add(24);
+employeeIds.Add(26);
+
+if (employeeIds.Contains(21))
+    Console.WriteLine("21 was found!");
+
+var employIdsArray = employeeIds.ToArray();
+
+employeeIds.Clear();
